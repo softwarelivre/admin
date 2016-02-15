bdbbedda16c781a495f5fb7e10ee055a8d621c76
softwarelivre <inscricao@software.org>
2016-02-15 16:04:54 -0200

    Updated humize dictionary

--- app/modules/humanized.js
+++ app/modules/humanized.js
@@ -16,5 +16,27 @@
        'proponent-student': 'proponente estudante',
        'speaker':           'palestrante',
        'student':           'estudante'
+
+        // disability types
+        'none':     'Não',
+        'physical': 'Física',
+        'hearing':  'Auditiva',
+        'visual':   'Visual',
+        'mental':   'Mental',
+
+        // ocupation types
+        //'student': 'Estudante',
+        'private_employee': 'Funcionário',
+        'public_employee': 'Funcionário Público',
+        'businessman': 'Empresário',
+        'freelancer': 'Autônomo',
+
+        // education
+        'post_graduation_stricto': 'Mestrado/Doutorado',
+        'post_graduation_lato': 'Pós Graduação/Especialização',
+        'graduation': 'Ensino Superior Completo',
+        'graduation_incomplete': 'Ensino Superior Incompleto',
+        'secondary': 'Ensino Médio Completo',
+        'secondary_incomplete': 'Ensino Médio Incompleto'
     });
 })();
